8d57413b2cce9374fb83dd4863fbe7b4fafa235f
Adithya-vajrala <vajrlaadithya@gmail.com>
2025-07-27 11:44:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// When the page is scrolled, add "scrolled" class to navbar
+window.addEventListener('scroll', () => {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
